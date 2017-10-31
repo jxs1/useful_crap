d08bc47692ce96e5a285b7f67403d01b58b7eeac
Jon Sherry <jonsherry1@googlemail.com>
2017-10-31 14:29:33 +0000

    adds install_erlang_20.1

---
+++ install_erlang.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y gcc make build-essential autoconf m4 libncurses5-dev libssh-dev unixodbc-dev openjdk-8-jdk libwxgtk3.0-dev xsltproc fop
+wget http://www.erlang.org/download/otp_src_20.1.tar.gz
+tar -zvxf otp_src_20.1.tar.gz
+cd otp_src_20.1
+export ERL_TOP=`pwd`
+./configure
+make -j 1
+sudo make install
+echo "done"
