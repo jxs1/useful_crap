debd7df4b34277f45f3141797588cf447983b881
Jon Sherry <jonsherry1@googlemail.com>
2017-11-22 15:40:42 +0000

    Added install_archain_and_mine

---
+++ install_archain_and_mine.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y gcc make build-essential autoconf m4 libncurses5-dev libssh-dev unixodbc-dev openjdk-8-jdk libwxgtk3.0-dev xsltproc fop
+wget http://www.erlang.org/download/otp_src_20.1.tar.gz
+tar -zvxf otp_src_20.1.tar.gz
+cd otp_src_20.1
+export ERL_TOP=`pwd`
+./configure
+make -j 1
+sudo make install
+echo "-----INSTALLED ERLANG SUCCESFULLY-----\n"
+sudo apt-get install -y git
+git clone https://github.com/ArchainTeam/archain.git
+git pull
+echo "-----CLONED ARCHAIN REPO-----"
+echo "\n-----FINISHED INSTALLATION-----"
+echo "\n-----INITIATE MINING-----"
+./archain-server peer 178.62.4.18 mine
